7a7ea5f6cb101bb03fffd1d37a843e59098476b9
cloer <chltjrrb97@gmail.com>
2022-08-10 00:04:40 -0400

    FIX: container class moved (layout => children) for grid sidebar

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -6,9 +6,7 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <main className='flex-1'>
-        <div className='container mx-auto'>{children}</div>
-      </main>
+      <main className='flex-1'>{children}</main>
       <Footer />
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='container mx-auto'>
       <h1>Index</h1>
     </div>
   );
