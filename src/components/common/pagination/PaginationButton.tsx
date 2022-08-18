27e43ad7ba0a0aa5bcf096b468751b2d1b2d9496
cloer <chltjrrb97@gmail.com>
2022-08-17 22:46:16 -0400

    UPDATE: pagination -> merged pagination button components

--- src/components/common/pagination/PaginationButton.tsx
+++ src/components/common/pagination/PaginationButton.tsx
@@ -2,13 +2,39 @@ import type { FC } from 'react';
 import Link from 'next/link';
 
 interface Props {
-  num: number;
+  type: 'current' | 'nomal' | 'Previous' | 'Next';
+  pageNum: number;
 }
 
-const PaginationButton: FC<Props> = ({ num }) => {
+const PaginationButton: FC<Props> = ({ type, pageNum }) => {
   return (
-    <Link href={`/blog/page/${num}`} passHref>
-      <a className='pagination-btn-color py-2 px-3 leading-tight border'>{num}</a>
+    <Link href={`/blog/page/${pageNum}`} passHref>
+      <li>
+        {type === 'current' ? (
+          <a className='z-10 py-2 px-3 leading-tight border bg-gray-200 text-gray-700 border-gray-300'>{pageNum}</a>
+        ) : type === 'nomal' ? (
+          <a className='pagination-btn-color py-2 px-3 leading-tight border'>{pageNum}</a>
+        ) : (
+          <a className={`pagination-btn-color block py-2 px-3 leading-tight rounded-${type === 'Previous' ? 'l' : 'r'}-lg border`}>
+            <span className='sr-only'>{type}</span>
+            <svg aria-hidden='true' className='w-5 h-5' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+              {type === 'Previous' ? (
+                <path
+                  fillRule='evenodd'
+                  d='M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z'
+                  clipRule='evenodd'
+                ></path>
+              ) : (
+                <path
+                  fillRule='evenodd'
+                  d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                  clipRule='evenodd'
+                ></path>
+              )}
+            </svg>
+          </a>
+        )}
+      </li>
     </Link>
   );
 };
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const POSTS_PER_PAGE = 6;
-export const PAGINATION_LENGTH = 7;
+export const PAGINATION_LENGTH = 5;
 export const DATA_SOURCE = {
   blog: 'blog',
   project: 'project',
