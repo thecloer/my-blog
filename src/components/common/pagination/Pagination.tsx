360bd25fb4738685dc63c129e62a7efe0699e4f3
cloer <chltjrrb97@gmail.com>
2022-08-23 23:18:33 -0400

    UPDATE: pagination cursor-pointer

--- src/components/common/pagination/Pagination.tsx
+++ src/components/common/pagination/Pagination.tsx
@@ -10,7 +10,7 @@ const Pagination: FC<Props> = ({ currentPage, numPages }) => {
   const { paginationNumbers, isPreviousButton, isNextButton } = generatePaginationNumbers(currentPage, numPages);
 
   return (
-    <nav aria-label='Page navigation' className='mt-12 w-full flex justify-center'>
+    <nav aria-label='Page navigation' className='mt-12 w-full flex justify-center cursor-pointer'>
       <ul className='flex items-center -space-x-0'>
         {paginationNumbers.map((pageNum, i) => (
           <PaginationButton
