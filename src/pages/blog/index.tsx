fcb4ff39de598708a13236d5e0b4646fb43ccf48
cloer <chltjrrb97@gmail.com>
2022-08-27 12:19:12 -0400

    FIX: import - export => export {default, getStaticProps} form 'path'

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,5 +1,1 @@
-import { getStaticProps } from './page/[page_index]';
-import BlogPage from './page/[page_index]';
-
-export { getStaticProps };
-export default BlogPage;
+export { default, getStaticProps } from './page/[page_index]';
