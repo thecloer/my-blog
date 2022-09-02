d486298d3380953dab92d1b26c79b2929ea5a622
cloer <chltjrrb97@gmail.com>
2022-09-02 15:09:05 -0400

    FIX: add margin to ProjsctSearch

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -12,7 +12,7 @@ const ProjectHome: NextPage<Props> = ({ projects }) => {
     <div className='container-lg-62rem mx-auto px-8 md:px-0'>
       <main className='py-24'>
         {/* <ProjectSearch /> TODO: Project Search */}
-        <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-16'>
+        <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
           {projects.map((project, i) => (
             <ProjectCard key={i} project={project} />
           ))}
