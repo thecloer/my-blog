7d7e02b39f2387314f2f5480e8411cbfb1938b74
cloer <chltjrrb97@gmail.com>
2022-09-02 15:19:44 -0400

    ADD: /about and '/' redirect to '/about'

---
+++ src/pages/about.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const AboutPage: NextPage = () => {
+  return <div>AboutPage</div>;
+};
+
+export default AboutPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,1 @@
-import type { NextPage } from 'next';
-
-const Home: NextPage = () => {
-  return (
-    <div className='container mx-auto'>
-      <h1>Index</h1>
-    </div>
-  );
-};
-
-export default Home;
+export { default } from './about';
