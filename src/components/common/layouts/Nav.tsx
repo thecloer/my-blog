4068cbf537bcd4a4c6ef0fd09e999c486bc596cf
cloer <chltjrrb97@gmail.com>
2022-09-02 15:18:47 -0400

    UPDATE: Nav delete NavButton and change nav-menu name portfolio -> about

--- src/components/common/layouts/Nav.tsx
+++ src/components/common/layouts/Nav.tsx
@@ -1,11 +1,13 @@
-import NavButton from './NavButton';
+import Link from 'next/link';
 
 const Nav = () => {
-  const navList = ['portfolio', 'project', 'blog', 'resume'];
+  const navList = ['about', 'project', 'blog', 'resume'];
   return (
     <nav className='flex'>
-      {navList.map((navText, i) => (
-        <NavButton text={navText} key={i} />
+      {navList.map((text, i) => (
+        <Link href={`/${text}`} passHref key={i}>
+          <a className='ml-5 cursor-pointer text-slate-500 hover:text-slate-700 transition-colors capitalize'>{text}</a>
+        </Link>
       ))}
     </nav>
   );
